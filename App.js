d2fbde4e23f1feb962a55e6dead3bfb54c659553
Martin Žilák <martin.zilak@artinsolutions.com>
2020-12-22 20:06:54 +0100

    Splitting the game into multiple screens - work in progress.

--- App.js
+++ App.js
@@ -1,79 +1,23 @@
-import React, { useEffect } from 'react';
-import {
-  SafeAreaView,
-  View,
-  Text,
-  StyleSheet,
-} from 'react-native';
-import Hive from './components/Hive';
+import React from 'react';
+import { createStackNavigator } from '@react-navigation/stack';
+import { NavigationContainer } from '@react-navigation/native';
+import { GameSizeProvider } from './contexts/GameSizeContext';
+import GameScreen from './screens/GameScreen';
 import { HIVE_SIZE } from './constants/constants';
-import HexagonButton from './components/HexagonButton';
-import { useGameStateControl } from './hooks/useGameStateControl';
 
-const App = () => {
-  const gameSize = HIVE_SIZE.SMALL;
+const Stack = createStackNavigator();
 
-  const {
-    grid,
-    beesRemaining,
-    score,
-    resetGame,
-    flagCell,
-    revealCell,
-  } = useGameStateControl(gameSize);
-
-  useEffect(() => {
-    resetGame();
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [/* component did mount */]);
-
-  return (
-    <>
-      <SafeAreaView style={styles.safeAreaView}>
-        <View style={styles.scoreView}>
-          <Text style={styles.scoreText}>
-            {`SCORE ${score}`}
-          </Text>
-
-          <Text style={styles.scoreText}>
-            {`BEES ${beesRemaining}`}
-          </Text>
-        </View>
-        
-        <Hive
-            grid={grid}
-            gameSize={gameSize}
-            revealCell={revealCell}
-            flagCell={flagCell}
-        />
-
-        <HexagonButton
-            onPress={() => resetGame()}
-            text={'reset'}
-        />
-      </SafeAreaView>
-    </>
-  );
-};
-
-const styles = StyleSheet.create({
-  safeAreaView: {
-    flex: 1,
-    justifyContent: 'space-between',
-    alignItems: 'center',
-  },
-  scoreView: {
-    flexDirection: 'row',
-    justifyContent: 'space-evenly',
-    alignItems: 'center',
-  },
-  scoreText: {
-    borderWidth: 1,
-    borderColor: 'red',
-    color: 'brown',
-    fontSize: 28,
-    fontWeight: '600',
-  },
-});
+const App = () => (
+    <NavigationContainer>
+        <GameSizeProvider value={HIVE_SIZE.SMALL}>
+            <Stack.Navigator>
+                {/*<Stack.Screen name="" component={MainMenuScreen} />*/}
+                {/*<Stack.Screen name="NewGameSize" component={NewGameSizeScreen} />*/}
+                <Stack.Screen name="Game" component={GameScreen} />
+                {/*<Stack.Screen name="PauseMenu" component={PauseMenuScreen} />*/}
+            </Stack.Navigator>
+        </GameSizeProvider>
+    </NavigationContainer>
+);
 
 export default App;
---
+++ contexts/GameSizeContext.js
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export const GameSizeContext = React.createContext({});
+
+export const GameSizeProvider = GameSizeContext.Provider;
