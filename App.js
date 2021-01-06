39d92a7acd054cd716057fe23e3ae298d247a50d
Martin Žilák <martin.zilak@artinsolutions.com>
2021-01-06 14:00:58 +0100

    Updated background image for a bigger one, added iOS app icon and splash screen.

--- App.js
+++ App.js
@@ -1,6 +1,7 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { createStackNavigator } from '@react-navigation/stack';
 import { NavigationContainer } from '@react-navigation/native';
+import SplashScreen from 'react-native-splash-screen';
 import { GameSettingsProvider } from './contexts/GameSettingsContext';
 import MainMenuScreen from './screens/MainMenuScreen';
 import NewGameSizeScreen from './screens/NewGameSizeScreen';
@@ -9,21 +10,27 @@ import { Screen } from "./constants/Screen";
 
 const Stack = createStackNavigator();
 
-const App = () => (
-    <NavigationContainer>
-        <GameSettingsProvider>
-            <Stack.Navigator
-                initialRouteName={Screen.MAIN_MENU}
-                screenOptions={{
-                    headerShown: false,
-                }}
-            >
-                <Stack.Screen name={Screen.MAIN_MENU} component={MainMenuScreen} />
-                <Stack.Screen name={Screen.NEW_GAME_SIZE} component={NewGameSizeScreen} />
-                <Stack.Screen name={Screen.GAME} component={GameScreen} />
-            </Stack.Navigator>
-        </GameSettingsProvider>
-    </NavigationContainer>
-);
+const App = () => {
+    useEffect(() => {
+        SplashScreen.hide();
+    }, []);
+
+    return (
+        <NavigationContainer>
+            <GameSettingsProvider>
+                <Stack.Navigator
+                    initialRouteName={Screen.MAIN_MENU}
+                    screenOptions={{
+                        headerShown: false,
+                    }}
+                >
+                    <Stack.Screen name={Screen.MAIN_MENU} component={MainMenuScreen} />
+                    <Stack.Screen name={Screen.NEW_GAME_SIZE} component={NewGameSizeScreen} />
+                    <Stack.Screen name={Screen.GAME} component={GameScreen} />
+                </Stack.Navigator>
+            </GameSettingsProvider>
+        </NavigationContainer>
+    );
+};
 
 export default App;
