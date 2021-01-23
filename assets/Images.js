ccdadce7fe32aecbdf67d0243a681261371cc995
Martin Žilák <martin.zilak@artinsolutions.com>
2021-01-23 14:42:26 +0100

    Changed bee and flag assets back to png to avoid webp aliasing.

--- assets/Images.js
+++ assets/Images.js
@@ -1,4 +1,4 @@
-export const BEE = require('./images/bee.webp');
-export const FLAG = require('./images/flag.webp');
+export const BEE = require('./images/bee.png');
+export const FLAG = require('./images/flag.png');
 export const BACKGROUND = require('./images/background.webp');
 export const LOGO = require('./images/logo.webp');
